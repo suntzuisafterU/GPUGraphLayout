309fd49ea92eeec124619275142a058377c2b5d1
adt150 <adt150@mail.usask.ca>
2019-06-10 18:17:31 -0600

    debug cpu version

--- builds/linux/debug_command.sh
+++ builds/linux/debug_command.sh
@@ -1,1 +1,1 @@
-gdb -tui --args graph_viewer gpu 10 1 wg 2.0 1.0 approximate ../../datasets/ca-AstroPh/out.ca-AstroPh out/temp_debugging_ca-AstroPh
+gdb -tui --args graph_viewer cpu 10 1 wg 2.0 1.0 approximate ../../datasets/ca-AstroPh/out.ca-AstroPh out/temp_debugging_ca-AstroPh
