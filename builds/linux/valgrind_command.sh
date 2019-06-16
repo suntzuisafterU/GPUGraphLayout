66dc0e4bfdfa05754bc750b751a7a785d23434da
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-06-16 00:58:54 -0500

    restructured repo

--- builds/linux/valgrind_command.sh
+++
@@ -1,1 +0,0 @@
-valgrind -s --track-origins=yes --leak-check=full --show-leak-kinds=all graph_viewer cpu 6 2 wg 2.0 1.0 approximate ../../datasets/ca-AstroPh/out.ca-AstroPh out/TEST_ca-AstroPh
---
+++ utils/gcov_command.sh
@@ -0,0 +1,1 @@
+gcov -d -o ../builds/linux/obj/ -s ../src/ ../builds/linux/graph_viewer cpu 20 1 sg 40.0 1.0 approximate ../datasets/amazon0601/out.amazon0601 ../out/
