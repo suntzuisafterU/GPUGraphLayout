8e1ed97876346cb77422e13dd6b29de429afce17
adt150 <adt150@mail.usask.ca>
2019-06-18 13:22:51 -0600

    Makefile and script updated.  Testscript and graph_viewer updated to accomodate 1/10th time on comm layout

--- utils/test_graph_viewer.sh
+++ utils/test_graph_viewer.sh
@@ -1,10 +1,11 @@
 ../builds/linux/graph_viewer \
-  cpu \
-  6 \
-  2 \
+  gpu \
+  501 \
+  12 \
   wg \
   2.0 \
   1.0 \
   approximate \
   ../datasets/ca-AstroPh/out.ca-AstroPh \
   ../out/TEST_ca-AstroPh \
+  png 10000 10000
