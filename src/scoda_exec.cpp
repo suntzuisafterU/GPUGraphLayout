005e745bfd2b0c235d5d10f38381ca0f85669001
adt150 <adt150@mail.usask.ca>
2019-07-04 17:29:01 -0600

    minor, notes

--- src/scoda_exec.cpp
+++ src/scoda_exec.cpp
@@ -5,7 +5,16 @@ using namespace std;
 namespace { // anonymous
 
 int main(int argc, const char** argv) {
+    // Accept edge stream from stdin
+
+    // Initialize datastructures and invoke scoda
+
+    // Send partition data to stdout
+
+    // TODO: scoda: produce other output? comm_graph output?
+
     cout << "In scoda_exec.cpp" << endl;
+    return EXIT_SUCCESS;
 }
 
 } // namespace anonymous
