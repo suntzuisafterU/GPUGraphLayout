fb1259a0a6b70cc7215e82a170dc8f769cf1f0ee
adt150 <adt150@mail.usask.ca>
2019-07-03 23:44:36 -0600

    scoda stream reverted

--- src/scoda/scoda.hpp
+++ src/scoda/scoda.hpp
@@ -10,13 +10,13 @@
 #include <sstream> // for istringstream() in scoda.cpp
 #include <iostream>
 
-#include "RPGraph.hpp" // For UGraph and nid_t
+#include "../RPGraph.hpp" // For UGraph and nid_t
 
 namespace CommunityAlgos {
     /**
      *
      */
-    int scoda_G(uint32_t degree_threshold, std::istream& edgelist_file,
+    int scoda_G(uint32_t degree_threshold, std::fstream& edgelist_file,
                 RPGraph::UGraph& full_graph, RPGraph::UGraph& comm_graph, 
                 std::unordered_map<RPGraph::nid_t, RPGraph::nid_t>& nid_comm_vec);
 
@@ -24,8 +24,7 @@ namespace CommunityAlgos {
                 RPGraph::UGraph& full_graph, RPGraph::UGraph& comm_graph, 
                 std::unordered_map<RPGraph::nid_t, RPGraph::nid_t>& nid_comm_vec);
 
-    int scoda_partition(uint32_t degree_threshold, std::istream &edgelist_file,
-                    std::unordered_map<RPGraph::nid_t, RPGraph::nid_t> &nid_comm_map);
+    int scoda_partition(uint32_t degree_threshold, std::fstream &edgelist_file);
     
     void print_partition(std::unordered_map<RPGraph::nid_t, RPGraph::nid_t> &nid_comm_map);
 }
