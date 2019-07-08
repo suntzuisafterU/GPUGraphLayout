b01c61b44267bab7e1eecff937707ad3831d8058
adt150 <adt150@mail.usask.ca>
2019-07-08 16:26:40 -0600

    make fast

--- utils/make_gv.sh
+++ utils/make_gv.sh
@@ -4,6 +4,7 @@ SCRIPT_NAME="$0"
 USAGE="Usage: \n $SCRIPT_NAME [-g | --debug] [--(no-)cuda] [--clean]\nClean and then compile all objects from source plus the main executable for graph_viewer. "
 
 TARGET=all
+FAST=0 # O0 optimization
 DEBUG=0
 CUDA_SUPPORT=1
 
@@ -26,6 +27,10 @@ do op="$1"
       CLEAN=1
       shift
       ;;
+    --fast )
+      FAST=1
+      shift
+      ;;
     --gv )
       TARGET=graph_viewer
       shift
@@ -45,7 +50,7 @@ if [ "$CLEAN" ];then
   make clean -C ../builds/linux
 fi
 
-make -k -C ../builds/linux CUDA_SUPPORT=$CUDA_SUPPORT DEBUG=$DEBUG $TARGET
+make -k -C ../builds/linux CUDA_SUPPORT=$CUDA_SUPPORT DEBUG=$DEBUG FAST=$FAST $TARGET
 
 echo -e "\nExecutables exist(may have just been made):" \
 && ls -1 --color=always ../builds/linux/graph_viewer ../builds/linux/scoda_exec ../builds/linux/stress_exec
