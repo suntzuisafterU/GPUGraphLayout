3c961d619fe84515c5ba5f79aac60144184bd8c4
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-07-10 01:10:47 -0600

    Incomplete SCoDA test, not part of makefile yet

---
+++ tests/scoda_tests.cpp
@@ -0,0 +1,6 @@
+#include "../scoda/scoda.hpp"
+#include "catch.hpp"
+
+TEST_CASE( "SCoDA reads small file (pass)", "[single-file]") {
+  REQUIRE( RPGraph::scoda_G(...// TODO: Write and fill in test cases. ))
+}
