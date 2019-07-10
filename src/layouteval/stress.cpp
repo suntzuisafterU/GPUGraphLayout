956bc437dec6e605af17a030b0a5e99a6fa770b8
adt150 <adt150@mail.usask.ca>
2019-07-10 17:03:07 -0600

    WIP: stress interface updated

--- src/layouteval/stress.cpp
+++ src/layouteval/stress.cpp
@@ -1,9 +1,8 @@
 #include "stress.hpp"
 
-namespace LayoutEval {
+namespace RPGraph {
 
 using RPGraph::Coordinate;
-using RPGraph::nid_t;
 using std::vector;
 
 /**
@@ -22,20 +21,14 @@ float stress(RPGraph::GraphLayout* layout, int L) {
     return 0.0F; // TODO: Temp
 }
 
-inline float distU(Coordinate c1, Coordinate c2) {
+float distU(Coordinate c1, Coordinate c2) {
     // OR: Just use the provided getDistance(con_nidt, con_nidt) method.
     // TODO: Euclidean distance
 }
 
-inline float distG() {
+float distG() {
     // TODO: shortest path distance.
 }
-
-vector<float> dijkstras() {
-    // TODO: Might do
-}
-
-
 
 /**
  * O(N**2) memory complexity, O(N**3)?? time complexity. 
@@ -45,4 +38,4 @@ vector< vector< float > >* allPairsShortestPaths(RPGraph::Graph* graph) {
     // TODO: Basically Dijkstras algo on each node individually. OR: floyd-warshall algo. Probably better.
 }
 
-} // LayoutEval
+} // RPGraph
--- src/layouteval/stress.hpp
+++ src/layouteval/stress.hpp
@@ -8,6 +8,18 @@ namespace LayoutEval {
 
 float stress(RPGraph::GraphLayout* layout, int L);
 
+inline float distU(Coordinate c1, Coordinate c2);
+
+inline float distG();
+
+/**
+ * O(N**2) memory complexity, O(N**3)?? time complexity. 
+ * Maybe just do Dijkstras
+ */
+vector< vector< float > >* allPairsShortestPaths(RPGraph::Graph* graph) {
+    // TODO: Basically Dijkstras algo on each node individually. OR: floyd-warshall algo. Probably better.
+}
+
 } // LayoutEval
 
 #endif // stress_hpp
