28108a6157770b6a2742b75a9e3b36ab79beb0d5
adt150 <adt150@mail.usask.ca>
2019-07-11 01:02:06 -0600

    WIP: GraphViewer class

---
+++ src/graph_viewer/RPGraphViewer.cpp
@@ -0,0 +1,168 @@
+
+
+
+
+
+// TODO: Move much of lambdas etc from graph viewer here.
+
+
+
+/*
+ ==============================================================================
+
+ graph_viewer.cpp
+ Copyright © 2016, 2017, 2018  G. Brinkmann
+
+ This file is part of graph_viewer.
+
+ graph_viewer is free software: you can redistribute it and/or modify
+ it under the terms of version 3 of the GNU Affero General Public License as
+ published by the Free Software Foundation.
+
+ graph_viewer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with graph_viewer.  If not, see <https://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------------
+
+ This code was written as part of a research project at the Leiden Institute of
+ Advanced Computer Science (www.liacs.nl). For other resources related to this
+ project, see https://liacs.leidenuniv.nl/~takesfw/GPUNetworkVis/.
+
+ ==============================================================================
+*/
+
+// Reading May 21th
+
+namespace RPGraph {
+
+    using namespace RPGraph::GraphViewer;
+    
+            GraphViewer(const bool cuda_requested,
+                        const int max_iterations,
+                        const int num_screenshots,
+                        const bool strong_gravity,
+                        const float scale,
+                        const float gravity,
+                        const bool approximate,
+                        const bool use_linlog,
+                        const float percentage_iterations_on_comm_graph,
+                        const char *edgelist_path,
+                        const char *out_path,
+                        const char *out_file_prefix,
+                        std::string out_format = "png",
+                        int image_w = 1250,
+                        int image_h = 1250 ){
+
+                        }
+
+
+            void GraphViewer::init(std::string edgelist_path) {
+                // TODO: Create a DatasetAdapter to the desired path (once DA is ready)
+                RPGraph::UGraph full_graph(edgelist_path); // Initialize full_graph from provided path.
+            }
+
+            void GraphViewer::show() {
+                // Display according to selected method.
+
+            }
+
+            void GraphViewer::layout(int num_iters) {
+                // Create the GraphLayout and ForceAtlas2 objects.
+                RPGraph::GraphLayout current_layout(comm_graph); /* Produce initial layout from comm_graph. */
+                RPGraph::ForceAtlas2* fa2; // Could be CPU or GPU object.
+                bool randomize = true;
+                #ifdef __NVCC__
+                if(cuda_requested)
+                    // GPU FA2
+                    fa2 = new RPGraph::CUDAForceAtlas2(comm_layout, approximate,
+                                                    strong_gravity, gravity, scale, randomize, use_linlog);
+                else
+                #endif
+                    fa2 = new RPGraph::CPUForceAtlas2(comm_layout, approximate,
+                                                    strong_gravity, gravity, scale, randomize, use_linlog);
+
+                const int snap_period = ceil((float)max_iterations/num_screenshots);
+                const int print_period = ceil((float)max_iterations*0.05);
+
+                /**
+                * Initial layout will be produced from community graph.
+                */
+                for (int iteration = 1; iteration < num_iters; ++iteration) // TODO: Should GraphViewer keep track of overall iterations??
+                {
+                    compositeStep(iteration); /* comm graph layout is produced. */
+                }
+
+                delete fa2;
+                /**
+                * Lambda function using capture-by-reference to access all outer scope 
+                * variables with the same symbol.
+                * https://stackoverflow.com/questions/4324763/can-we-have-functions-inside-functions-in-c
+                */
+                auto produceOutput = [&](int iteration) {
+                    /**
+                    * Reverted to older version after multiple issues with the line intended to extract the basename of the network
+                    */
+                    std::string op(out_path);
+                    op.append("/").append(out_file_prefix).append(std::to_string(iteration)).append(".").append(out_format);
+                    printf("Starting iteration %d (%.2f%%), writing %s...", iteration, 100 * (float)iteration / max_iterations, out_format.c_str());
+                    fflush(stdout);
+                    fa2->sync_layout();
+
+                    // if (out_format == "png")
+                        writeToPNG(current_layout, image_w, image_h, op);
+
+                    printf("done.\n");
+                };
+
+                auto compositeStep = [&](int iteration) {
+                    /**
+                    * Implementation in either RPGPUForceAtlas2.cu or RPCPUForceAtlas2.cpp
+                    */
+                    fa2->doStep();
+                    // If we need to, write the result to a png
+                    if (num_screenshots > 0 && (iteration % snap_period == 0 || iteration == max_iterations))
+                    {
+                        produceOutput(iteration);
+                    }
+
+                    // Else we print (if we need to)
+                    else if (iteration % print_period == 0)
+                    {
+                        printf("Starting iteration %d (%.2f%%).\n", iteration, 100*(float)iteration/max_iterations);
+                    }
+                };
+
+            }
+
+            void GraphViewer::compress() {
+                // Create new comm_map and graph, add each to container.
+                std::unordered_map<RPGraph::nid_t, RPGraph::nid_t> nid_comm_map; /**< Map is used since node_ids are not necessarily sequentially complete. Stack allocation. */
+                RPGraph::UGraph comm_graph;                // Initialize empty comm_graph for scoda to fill.
+                // run CommunityAlgo
+                uint32_t degree_threshold = 2; // TODO: TEMP VALUE TO TEST COMPILING, should figure out some way to parameterize or detect this in the future.  Note that detection requires streaming the entire graph with the authors implementation.  We could try sampling from the first portion of the graph (say 10%) and using a default value up to that point.
+                // TODO: Implement move assignment.
+                RPGraph::SCoDA_Results = RPGraph::scoda_G(degree_threshold, full_graph, comm_graph, nid_comm_map); /**< Currently the streaming algorithm is required to also initialize any UGraph datastructures that are required. */
+
+            }
+
+            /**
+            * Expansion:
+            *   Use setCoordinates(node_id, coordinate(x,y)); to expand the community layout to a full graph layout.
+            */
+            void GraphViewer::expand() {
+            auto nid_comm_map = nid_comm_maps.pop();
+                for (const auto& nid_commid_pair : nid_comm_map) {
+                    RPGraph::nid_t node = nid_commid_pair.first;
+                    RPGraph::nid_t comm = nid_commid_pair.second;
+                    RPGraph::Coordinate comm_coordinate = comm_layout.getCoordinate(comm_graph.node_map[comm]);
+                    // TODO: Is it possible for a node to not have a community in the graph??? Probably yes. Does not seem to be an issue.
+                    full_layout.setCoordinates(full_graph.node_map[node], comm_coordinate); /**< Set the nodes id to be that of it's community. */
+                }
+            }
+
+} // namespace RPGraph
---
+++ src/graph_viewer/RPGraphViewer.hpp
@@ -0,0 +1,80 @@
+#ifndef RPGraphViewer_hpp
+#define RPGraphViewer_hpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <math.h>
+#include <fstream>
+
+#include "../common/RPCommon.hpp"
+#include "../common/RPGraph.hpp"
+#include "../common/RPGraphLayout.hpp"
+#include "../common/RPTypeDefs.hpp"
+#include "../utils/IO/RPGraphLayoutUtils.hpp"
+#include "../FA2/RPCPUForceAtlas2.hpp"
+#include "../scoda/DisjointPartitionAlgo.hpp"
+#include "../scoda/scoda.hpp"
+
+#ifdef __NVCC__
+#include <cuda_runtime_api.h>
+#include "../FA2/RPGPUForceAtlas2.hpp"
+#endif // __NVCC__
+
+
+namespace RPGraph {
+    class GraphViewer {
+        public:
+            GraphViewer(const bool cuda_requested,
+                        const int max_iterations,
+                        const int num_screenshots,
+                        const bool strong_gravity,
+                        const float scale,
+                        const float gravity,
+                        const bool approximate,
+                        const bool use_linlog,
+                        const float percentage_iterations_on_comm_graph,
+                        const char *edgelist_path,
+                        const char *out_path,
+                        const char *out_file_prefix,
+                        std::string out_format = "png",
+                        int image_w = 1250,
+                        int image_h = 1250 );
+
+            GraphViewer() = delete;
+            // GraphViewer(OutputMethod output_method); // TODO: Use after testing.
+            GraphViewer(const GraphViewer& other) = delete;
+            GraphViewer & operator=(const GraphViewer& other) = delete;
+            void init(std::string file_path);
+            void show(); /**< Display or print data, depends on output method. */
+
+
+        private:
+            std::vector<UGraph> graphs; // TODO: Would a stack be better here?
+            std::unordered_map<RPGraph::contiguous_nid_t, RPGraph::contiguous_nid_t> nid_comm_maps;
+            RPGraph::DisjointPartitionAlgo comm_algo;
+
+            /// Parameters to layout algorithms.
+            const bool cuda_requested;
+            const int max_iterations;
+            const int num_screenshots;
+            const bool strong_gravity;
+            const float scale;
+            const float gravity;
+            const bool approximate;
+            const bool use_linlog;
+            const float percentage_iterations_on_comm_graph;
+            const char *edgelist_path;
+            const char *out_path;
+            const char *out_file_prefix;
+            std::string out_format;
+            int image_w;
+            int image_h;
+
+    }
+}
+
+
+
+
+#endif // RPGraphViewer_hpp
