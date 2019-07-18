3740fa73b64b6ca0fde706a864aeba2529fa9ec3
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-07-18 11:58:38 -0600

    WIP: IMPORTANT: Added BOOST_STRONG_TYPEDEF

--- src/common/RPDangerousTypeDefs.hpp
+++ src/common/RPDangerousTypeDefs.hpp
@@ -1,11 +1,13 @@
 #ifndef RPDangerousTypeDefs_hpp
 #define RPDangerousTypeDefs_hpp
 
+#include <boost/serialization/strong_typedef.hpp>
+
 namespace RPGraph {
 
 	// typedefs do not provide type safety: https://stackoverflow.com/questions/9588009/typesafe-typedef-in-c
 	// Using boost macro instead: https://www.boost.org/doc/libs/1_49_0/libs/serialization/doc/strong_typedef.html
-	typedef uint32_t dangerous_nid_t; /// IMPORTANT: For identifiying when a node id corresponds to the external dataset node id (OR to a previous graphs community node in SCoDA)
+	BOOST_STRONG_TYPEDEF(uint32_t, dangerous_nid_t); /// IMPORTANT: For identifiying when a node id corresponds to the external dataset node id (OR to a previous graphs community node in SCoDA)
 
 } // namespace RPGraph
 
--- src/common/RPTypeDefs.hpp
+++ src/common/RPTypeDefs.hpp
@@ -2,6 +2,7 @@
 #define RPTypeDefs_hpp
 
 #include <unordered_map>
+#include <boost/serialization/strong_typedef.hpp>
 
 namespace RPGraph {
 
@@ -13,10 +14,10 @@ namespace RPGraph {
 
 // Type to represent node IDs.
 // NOTE: we limit to 4,294,967,296 nodes through uint32_t.
-typedef uint32_t contiguous_nid_t;
+BOOST_STRONG_TYPEDEF(uint32_t, contiguous_nid_t);
 
 // Type to represent community ids.
-typedef uint32_t comm_id_t;
+BOOST_STRONG_TYPEDEF(uint32_t, comm_id_t);
 
 // Container for mapping contiguous_nid_t to comm_ids
 typedef std::unordered_map<RPGraph::contiguous_nid_t, RPGraph::comm_id_t> nid_comm_map_t;
