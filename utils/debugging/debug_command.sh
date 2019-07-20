a871dcd2b59c200c8851a946f373191712d6d642
adt150 <adt150@mail.usask.ca>
2019-07-19 18:33:17 -0600

    WIP: Makefile updated

--- utils/debugging/debug_command.sh
+++ utils/debugging/debug_command.sh
@@ -1,1 +1,1 @@
-gdb -tui --args ../../builds/linux/graph_viewer cpu 10 1 wg 2.0 1.0 approximate ../datasets/ca-AstroPh/out.ca-AstroPh ../out/temp_debugging_ca-AstroPh
+gdb --args ../../builds/linux/graph_viewer_exec cpu 10 1 wg 2.0 1.0 approximate regular 55 ../../datasets/ca-AstroPh/out.ca-AstroPh ../junk_out/temp_debugging_ca-AstroPh
