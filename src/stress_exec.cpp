ef5879335b6c7c19c204c99bc12cc70e6ee8d6b2
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-07-23 23:42:52 -0600

    WIP: minor

--- src/stress_exec.cpp
+++ src/stress_exec.cpp
@@ -9,17 +9,17 @@
 int main(int argc, const char** argv) {
 	// Accept layout file?
 	std::string edge_list = argv[1];
-	std::string layout_file = argv[2];
+	std::string layout_path = argv[2];
 
 	RPGraph::UGraph graph = new RPGraph::UGraph(edge_list);
 	RPGraph::GraphLayout layout(graph);
 
 	// TODO: Read layout file and update all coordinates in layout.
-	std::fstream layout_stream(layout_file, std::ifstream::in);
+	std::fstream layout_stream(layout_path, std::ifstream::in);
 
 	RPGraph::contiguous_nid_t curr_node;
 	std::string line;
-	while(std::getline(layout_file, line))
+	while(std::getline(layout_stream, line))
 	{
 		// Skip any comments
 		if(line[0] == '#') continue;
@@ -33,7 +33,7 @@ int main(int argc, const char** argv) {
 
 		curr_node++;
 	}
-	layout_file.close();
+	layout_stream.close();
 
 	// TODO: Call stress algo.
 	
