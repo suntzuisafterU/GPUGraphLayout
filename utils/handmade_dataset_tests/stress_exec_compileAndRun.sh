601379a289daa8b5694057e833e7ea07e1005fa5
adt150 <adt150@mail.usask.ca>
2019-07-25 13:00:39 -0600

    minor

--- utils/handmade_dataset_tests/stress_exec_compileAndRun.sh
+++ utils/handmade_dataset_tests/stress_exec_compileAndRun.sh
@@ -1,4 +1,4 @@
-LAYOUTFILE="ChangeMe"
+LAYOUTFILE=test_dataset1_layouts_origGV/100.csv
 cd ../../builds/linux && make clean && make DEBUG=1 stress_exec;
 cd ../../utils/handmade_dataset_tests/
 ../../builds/linux/stress_exec test_dataset1.edges $LAYOUTFILE
