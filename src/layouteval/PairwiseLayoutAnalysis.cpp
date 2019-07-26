0ef69886499341219eaafb5ea470cfb9e9c23df5
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-07-26 12:00:01 -0600

    Pairwise layout analysis.  Has to be compiled and tested.

--- src/layouteval/PairwiseLayoutAnalysis.cpp
+++ src/layouteval/PairwiseLayoutAnalysis.cpp
@@ -1,23 +1,58 @@
 #include "PairwiseLayoutAnalysis.hpp"
 
+#include <cmath>
 #include <iostream>
+#include <limits>
+
+#include "../common/RPGraph.hpp"
+#include "../common/RPTypeDefs.hpp"
 
 namespace RPGraph {
 
-	PairwiseLayoutReport computePairwiseCoordinateError(GraphLayout& layout1, GraphLayout& layout2) {
+	double EuclideanDistance(Coordinate c1, Coordinate c2) {
+		double result{ std::sqrt(std::pow(c1.x - c2.x, 2) + std::pow(c1.y - c2.y, 2)) };
+		return result;
+	};
+
+	PairwiseLayoutReport computePairwiseCoordinateError(GraphLayout& correct_layout, GraphLayout& community_expanded_layout) {
 		/* Ensure layouts are from the same graph. */
-		if (layout1.graph != layout2.graph) {
+		if (correct_layout.graph != community_expanded_layout.graph) {
 			std::cout << "ERROR: Trying to compare layouts of different graphs!!" << std::endl;
 			exit(EXIT_FAILURE);
 		}
-		
-		/* Iterate over nodes, get coordinates of each, compute the difference and RMSE. */
-		uint32_t N{ layout1.graph.num_nodes() };
-		for (uint32_t i = 0; i < N; ++i) {
 
+		/* Iterate over nodes, get coordinates of each, compute the difference and RMSE. */
+		uint32_t N{ correct_layout.graph.num_nodes() };
+		double total_error{ 0 };
+		double RMSE{ 0 };
+		double largest_error{ 0 };
+		double smallest_error{ std::numeric_limits<double>::infinity() };
+
+		for (contiguous_nid_t i = 0; i < N; ++i) {
+			Coordinate c1 = correct_layout.getCoordinateFromContig(i);
+			Coordinate c2 = community_expanded_layout.getCoordinateFromContig(i);
+			double Udist = EuclideanDistance(c1, c2);
+			if (Udist > largest_error) largest_error = Udist;
+			if (Udist < smallest_error) smallest_error = Udist;
+			total_error += Udist;
+			RMSE += std::pow(Udist, 2);
 		}
 
+		RMSE /= N;
+		double average_error{ total_error / N };
+
 		/* In report, consider the size of the layout. Also compute the RMSE. */
+		PairwiseLayoutReport report = {
+			correct_layout.graph.dataset_source(),
+			N,
+			total_error,
+			average_error,
+			largest_error,
+			smallest_error,
+			RMSE
+		};
+
+		return report;
 	}
 
 } // namespace RPGraph
--- src/layouteval/PairwiseLayoutAnalysis.hpp
+++ src/layouteval/PairwiseLayoutAnalysis.hpp
@@ -1,10 +1,33 @@
 #ifndef PairwiseLayoutAnalysis_hpp
 #define PairwiseLayoutAnalysis_hpp
 
+#include <iostream>
+#include <string>
+
+#include "../common/RPGraphLayout.hpp"
+
 namespace RPGraph {
 
     struct PairwiseLayoutReport {
-        float average_distances_delta; // TODO: What should actually be reported?
+		std::string dataset_source;
+		uint32_t num_nodes;
+		double total_error;
+        double average_error; // TODO: What should actually be reported?
+		double largest_error;
+		double smallest_error;
+		double RMSE;
+
+		friend std::ostream& operator<< (std::ostream& out, PairwiseLayoutReport& report) {
+			out << "##### Pairwise Layout Difference Report #####" << std::endl;
+			out << "# dataset source: " << report.dataset_source << std::endl;
+			out << "# num nodes: " << report.num_nodes << std::endl;
+			out << "# total error: " << report.total_error << std::endl;
+			out << "# average error: " << report.average_error << std::endl;
+			out << "# largest error: " << report.largest_error << std::endl;
+			out << "# smallest error: " << report.smallest_error << std::endl;
+			out << "# RMSE: " << report.RMSE << std::endl;
+			return out;
+		};
     };
 
 	PairwiseLayoutReport computePairwiseCoordinateError(GraphLayout& layout1, GraphLayout& layout2);
