cdb00a88d4042add3580f08d98c07dcba1408b00
adt150 <adt150@mail.usask.ca>
2019-07-25 19:06:28 -0600

    readFromCSV(), currently linker is having errors

--- src/utils/IO/RPGraphLayoutUtils.cpp
+++ src/utils/IO/RPGraphLayoutUtils.cpp
@@ -1,5 +1,8 @@
 #include "RPGraphLayoutUtils.hpp"
 
+#include <sstream>
+#include <fstream>
+
 namespace RPGraph {
     /**
      * pngwriter entry point.
@@ -69,6 +72,51 @@ namespace RPGraph {
          out_file.close();
      }
  
+
+    void readFromCSV(RPGraph::GraphLayout& layout, std::string layout_path) {
+		std::cout << "Reading the layout file and setting all coordinates within the layout." << std::endl;
+		// TODO: Read layout file and update all coordinates in layout.
+		std::ifstream layout_stream;
+		layout_stream.open(layout_path);
+		uint32_t N{ layout.graph.num_nodes() };
+
+		// This is code that reads a custom layout format. Specifically:
+		// node_id: uint32_t, x_coord: float, y_coord: float
+		// TODO: This would be best put somewhere else, for example in IO utils, if it were being used anywhere else.
+		uint32_t temp_counter = 0;
+		std::string line;
+		while (std::getline(layout_stream, line))
+		{
+			// Skip any comments
+			if (line[0] == '#') continue;
+			if (line[0] == '%') continue;
+
+			// Read source and target from file
+			RPGraph::dangerous_nid_t external_nid;
+            RPGraph::contiguous_nid_t safe_nid;
+			float x, y;
+			std::istringstream ss(line);
+			ss >> external_nid;
+            safe_nid = layout.graph.getContigFromExternal(external_nid); // IMPORTANT: Maps through associated graph.
+            if(safe_nid >= N) { // Safety check just in case.
+                std::cout << "ERROR: safe_nid == " << safe_nid << " and is larger than N = " << N << std::endl;
+                exit(EXIT_FAILURE);
+            }
+
+			if (ss.peek() == ',') ss.ignore();
+			ss >> x;
+			if (ss.peek() == ',') ss.ignore();
+			ss >> y;
+
+			layout.setCoordinates(safe_nid, RPGraph::Coordinate(x, y));
+
+			temp_counter++;
+		}
+	}
+
+
+	// std::cout << "About to close the layout file" << std::endl;
+	// layout_stream.close();
 //     /**
 //      * Do we have any use for writing to bin?
 //      */
--- src/utils/IO/RPGraphLayoutUtils.hpp
+++ src/utils/IO/RPGraphLayoutUtils.hpp
@@ -1,7 +1,7 @@
 #ifndef RPGraphLayoutUtils_hpp
 #define RPGraphLayoutUtils_hpp
 
-#include <fstream>
+#include <string>
 
 #include "../../common/RPGraphLayout.hpp"
 #include "../../../lib/pngwriter/src/pngwriter.h"
@@ -10,6 +10,7 @@
 namespace RPGraph {
     void writeToPNG(RPGraph::GraphLayout* layout, const int image_w, const int image_h, std::string path);
     void writeToCSV(RPGraph::GraphLayout* layout, std::string path);
+    void readFromCSV(RPGraph::GraphLayout& layout, std::string path);
     // void writeToBin(RPGraph::GraphLayout* layout, std::string path);
 } // namespace RPGraph
 
