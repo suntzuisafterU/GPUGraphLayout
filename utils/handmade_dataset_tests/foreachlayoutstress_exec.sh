cfa1bad8c572b7fb351f851460ff194d8ba105b0
adt150 <adt150@mail.usask.ca>
2019-07-26 17:10:56 -0600

    minor

--- utils/handmade_dataset_tests/foreachlayoutstress_exec.sh
+++ utils/handmade_dataset_tests/foreachlayoutstress_exec.sh
@@ -6,5 +6,5 @@ echo "$LAYOUT_DIR"
 
 for layout in $LAYOUT_DIR/*.csv; do
   echo -ne "\n\nExecuting stress on $layout\n"
-  ../../builds/linux/stress_exec "$EDGELIST_FILE" "$layout"
+  ./stress_exec "$EDGELIST_FILE" "$layout"
 done
