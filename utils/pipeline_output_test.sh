f64e85063bc97bfacf4b171ef51c757044406fe9
adt150 <adt150@mail.usask.ca>
2019-07-30 16:27:05 -0600

    Producing 10000px output

--- utils/pipeline_output_test.sh
+++ utils/pipeline_output_test.sh
@@ -1,6 +1,6 @@
 REASON=VERIFY_STACKED_COMPRESSION
-IN=../datasets/ca-AstroPh/out.ca-AstroPh
-# IN=../datasets/web-Google/out.web-Google
+# IN=../datasets/ca-AstroPh/out.ca-AstroPh
+IN=../datasets/web-Google/out.web-Google
 OUT=../out/pipeline_output_tests/$REASON/
 mkdir -p "$OUT"
-./execute_graph_viewer.sh --outfile-prefix "$REASON" --execution-mode gpu --append -f "$IN"  -o "$OUT" -v -r 3.0 -g 1.0 --num-iters 500 --num-snaps 1 --percent-comm 55 --annotate-path "pipeline_output_test" --pixels 1250 1250
+./execute_graph_viewer.sh --outfile-prefix "$REASON" --execution-mode gpu --append -f "$IN"  -o "$OUT" -v -r 3.0 -g 1.0 --num-iters 500 --num-snaps 1 --percent-comm 55 --annotate-path "pipeline_output_test" --pixels 10000 10000
