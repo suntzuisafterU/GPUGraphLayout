5b82ffde7b4d8bbac300c10ed1e646571b72eca3
adt150 <adt150@mail.usask.ca>
2019-08-01 21:15:13 -0600

    overloading operator+() on StressReport

--- src/layouteval/stress.hpp
+++ src/layouteval/stress.hpp
@@ -16,6 +16,24 @@ struct StressReport {
 	double stress_per_node;
 	uint32_t num_edges;
 	double stress_per_edge;
+
+	// override addition operator.
+	friend StressReport operator+(const StressReport& sr1, const StressReport& sr2) {
+		double combined_stress{ sr1.stress + sr2.stress };
+		uint32_t combined_num_nodes{ sr1.num_nodes + sr2.num_nodes };
+		double combined_stress_per_node{ combined_stress/combined_num_nodes };
+		uint32_t combined_num_edges{ sr1.num_edges + sr2.num_edges };
+		double combined_stress_per_edge{ combined_stress/combined_num_edges };
+
+		return StressReport {
+			combined_stress,
+			combined_num_nodes,
+			combined_stress_per_node,
+			combined_num_edges,
+			combined_stress_per_edge
+		};
+	}
+
     // TODO: Should this have some way to reference the graph? Parameters that were used, etc.
 
 	friend std::ostream& operator<<(std::ostream& out, StressReport& report) {
