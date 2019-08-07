72289d9f6f661e7cd980b108bdaf0d0d9dbd64e2
adt150 <adt150@mail.usask.ca>
2019-08-06 21:49:53 -0600

    stress script improved

--- experiments/preparation_stage/stress_of_real_datasets/stress_of_real_datasets.sh
+++ experiments/preparation_stage/stress_of_real_datasets/stress_of_real_datasets.sh
@@ -1,37 +1,44 @@
 
-# Accept dataset or point at directory.
-EDGELIST=datasets/ca-AstroPh/out.ca-AstroPh 
-BASENAME=$(basename -- "$EDGELIST")
-OUT_ORIGINAL=out/$BASENAME/original
-OUT_SCODA=out/$BASENAME/scoda
+TIMESTAMP=$(date +"%m%d%H%M")
+echo "Timestamp: $TIMESTAMP"
 
-# clean up any old output.
-echo "Removing any old output with: "
-echo "rm -rf out/$BASENAME"
-rm -rf out/$BASENAME
+for d in datasets/*/out.*
+do 
+  # Accept dataset or point at directory.
+  EDGELIST=$d
+  BASENAME=$(basename -- "$EDGELIST")
+  OUT_ORIGINAL=out/$BASENAME/original
+  OUT_SCODA=out/$BASENAME/scoda
 
-echo "Creating $OUT_ORIGINAL and $OUT_SCODA if they don't already exist"
-mkdir -p $OUT_ORIGINAL
-mkdir -p $OUT_SCODA
+  # # clean up any old output.
+  # echo "Removing any old output with: "
+  # echo "rm -rf out/$BASENAME"
+  # rm -rf out/$BASENAME
 
-# write reports to reports directory. For now clean out first.
-echo "Removing old reports with: "
-echo "rm -rf reports"
-rm -rf reports/$BASENAME
-echo "Creating reports directory..." && mkdir -p reports/$BASENAME
+  echo "Creating $OUT_ORIGINAL and $OUT_SCODA if they don't already exist"
+  mkdir -p $OUT_ORIGINAL
+  mkdir -p $OUT_SCODA
 
-# produce layouts for our use.
-# Execute with original mode.
-EXPERIMENT_TAG="_STRESS_EXPERIMENT_"
-# Usage: graph_viewer original|single|stacked gpu|cpu max_iterations num_snaps sg|wg scale gravity exact|approximate linlog|regular [percentage_iterations_on_comm_graph] edgelist_path out_path out_file_prefix [png image_w image_h|csv|bin]
+  # # write reports to reports directory. For now clean out first.
+  # echo "Removing old reports with: "
+  # echo "rm -rf reports"
+  # rm -rf reports/$BASENAME
+  echo "Creating reports directory..."
+  mkdir -p reports/$BASENAME
 
-./graph_viewer_exec "original" gpu 500 1 wg 5.0 1.0 approximate regular 55 $EDGELIST $OUT_ORIGINAL "$EXPERIMENT_TAG" csv | tee reports/"$BASENAME"/gv_original_output.txt 
-./graph_viewer_exec "single" gpu 500 1 wg 5.0 1.0 approximate regular 55 $EDGELIST $OUT_SCODA "$EXPERIMENT_TAG" csv | tee reports/"$BASENAME"/gv_scoda_pipeline_output.txt
+  # produce layouts for our use.
+  # Execute with original mode.
+  EXPERIMENT_TAG="_STRESS_EXPERIMENT_"
+  # Usage: graph_viewer original|single|stacked gpu|cpu max_iterations num_snaps sg|wg scale gravity exact|approximate linlog|regular [percentage_iterations_on_comm_graph] edgelist_path out_path out_file_prefix [png image_w image_h|csv|bin]
 
-# Usage: stress_exec [edge list path] [first generated layout path] [second generated layout path]
-LAYOUT1=$OUT_ORIGINAL/*"$EXPERIMENT_TAG"* 
-LAYOUT2=$OUT_SCODA/*FULL_LAYOUT*"$EXPERIMENT_TAG"*
-echo "./stress_rand_sampling $EDGELIST $LAYOUT1 $LAYOUT2 | tee reports/"$BASENAME"/stress_report.txt"
-./stress_rand_sampling $EDGELIST $LAYOUT1 $LAYOUT2 | tee reports/"$BASENAME"/stress_report.txt
+  ./graph_viewer_exec "original" gpu 500 1 wg 5.0 1.0 approximate regular 55 $EDGELIST $OUT_ORIGINAL "$EXPERIMENT_TAG" csv | tee reports/"$BASENAME"/gv_original_output_"$TIMESTAMP".txt 
+  ./graph_viewer_exec "single" gpu 500 1 wg 5.0 1.0 approximate regular 55 $EDGELIST $OUT_SCODA "$EXPERIMENT_TAG" csv | tee reports/"$BASENAME"/gv_scoda_pipeline_output_"$TIMESTAMP".txt
 
+  # Usage: stress_exec [edge list path] [first generated layout path] [second generated layout path]
+  LAYOUT1=$OUT_ORIGINAL/*"$EXPERIMENT_TAG"* 
+  LAYOUT2=$OUT_SCODA/*FULL_LAYOUT*"$EXPERIMENT_TAG"*
+  echo "./stress_rand_sampling $EDGELIST $LAYOUT1 $LAYOUT2 | tee reports/"$BASENAME"/stress_report.txt"
+  ./stress_rand_sampling $EDGELIST $LAYOUT1 $LAYOUT2 | tee reports/"$BASENAME"/stress_report.txt
+
+done
 
